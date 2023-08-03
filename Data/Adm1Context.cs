7abca2ba22289a57f69ac28f857c0fb6c07aea46
DHung308 <nguyenduonghung30082001@gmail.com>
2023-08-03 14:12:33 +0700

    Lan2

---
+++ Data/Adm1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Adm1.Models;
+
+namespace Adm1.Data
+{
+    public class Adm1Context : DbContext
+    {
+        public Adm1Context (DbContextOptions<Adm1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Adm1.Models.Student> Student { get; set; } = default!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Adm1.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<Adm1Context>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("Adm1Context") ?? throw new InvalidOperationException("Connection string 'Adm1Context' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
